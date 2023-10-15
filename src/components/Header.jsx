f2e960229cf43817d1ecab0519330750675d5844
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-10-16 00:23:28 +0400

    style change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className="w-screen h-12 flex justify-between align-middle items-center absolute bg-gradient-to-b from-neutral-900 ... text-neutral-100">
+    <div className="w-screen h-12 flex justify-between align-middle items-center absolute text-neutral-100">
       <h1 className="ml-6">MovieDB</h1>
       <nav>
         <a href="" className="mr-6">
