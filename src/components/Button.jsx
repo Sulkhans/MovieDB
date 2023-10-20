5d9830cf121ebd92ec4101cb81565c0ff715fe0b
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-10-20 23:22:13 +0400

    Added route functionality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import Arrow from "../assets/arrow.svg?react";
 
-export const Button = () => {
+export const Button = (props) => {
   return (
     <button className="flex items-center text-lg px-5 py-2 border-2 rounded-full border-white border-opacity-30 fill-white hover:bg-white hover:text-black hover:fill-black transition-all">
-      See more <Arrow className="w-5 h-5 ml-1 -mr-1" />
+      <Link to={props.path}>See more</Link>
+      <Arrow className="w-5 h-5 ml-1 -mr-1" />
     </button>
   );
 };
