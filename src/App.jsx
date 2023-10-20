12bf030d531e1d7d81c4e7cd1629ab2d73e75343
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-10-20 23:21:24 +0400

    Moved state

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,10 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 const App = () => {
-  const [latestMovies, setLatestMovies] = useState([{}]);
-  const [topMovies, setTopMovies] = useState([]);
-  const [topShows, setTopShows] = useState([]);
-  const [upcoming, setUpcoming] = useState([]);
-
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZDY5ZmY2YTMwOGRlOWRlNDBlMmMwMDlmYTlkMGFhYSIsInN1YiI6IjY1Mjk3NDJkMWYzZTYwMDBmZjg1MTRkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7zkmF3jayq5zSMH3TS2TsU9bvQNABU0myOj-lUjHkEk",
-      },
-    };
-    const url1 =
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
-    const url2 =
-      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
-    const url3 =
-      "https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1";
-    const url4 =
-      "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&region=us";
-
-    fetch(url1, options)
-      .then((res) => res.json())
-      .then((json) => setLatestMovies(json.results))
-      .catch((err) => console.error("error:" + err));
-
-    fetch(url2, options)
-      .then((res) => res.json())
-      .then((json) => setTopMovies(json.results))
-      .catch((err) => console.error("error:" + err));
-
-    fetch(url3, options)
-      .then((res) => res.json())
-      .then((json) => setTopShows(json.results))
-      .catch((err) => console.error("error:" + err));
-
-    fetch(url4, options)
-      .then((res) => res.json())
-      .then((json) => setUpcoming(json.results))
-      .catch((err) => console.error("error:" + err));
-  }, []);
-
   return (
     <div>
       <Header />
-      <Home
-        latestMovies={latestMovies}
-        topMovies={topMovies}
-        topShows={topShows}
-        upcoming={upcoming}
-      />
+      <Home />
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Slider } from "./Slider";
 import { Trending } from "./Trending";
 import { Section } from "./Section";
 
-export const Home = ({ latestMovies, topMovies, topShows, upcoming }) => {
+export const Home = () => {
   const [index, setIndex] = useState(0);
+  const [latestMovies, setLatestMovies] = useState([{}]);
+  const [topMovies, setTopMovies] = useState([]);
+  const [topShows, setTopShows] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZDY5ZmY2YTMwOGRlOWRlNDBlMmMwMDlmYTlkMGFhYSIsInN1YiI6IjY1Mjk3NDJkMWYzZTYwMDBmZjg1MTRkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7zkmF3jayq5zSMH3TS2TsU9bvQNABU0myOj-lUjHkEk",
+      },
+    };
+    const url1 =
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+    const url2 =
+      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+    const url3 =
+      "https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1";
+    const url4 =
+      "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&region=us";
+
+    fetch(url1, options)
+      .then((res) => res.json())
+      .then((json) => setLatestMovies(json.results))
+      .catch((err) => console.error("error:" + err));
+
+    fetch(url2, options)
+      .then((res) => res.json())
+      .then((json) => setTopMovies(json.results))
+      .catch((err) => console.error("error:" + err));
+
+    fetch(url3, options)
+      .then((res) => res.json())
+      .then((json) => setTopShows(json.results))
+      .catch((err) => console.error("error:" + err));
+
+    fetch(url4, options)
+      .then((res) => res.json())
+      .then((json) => setUpcoming(json.results))
+      .catch((err) => console.error("error:" + err));
+  }, []);
 
   return (
     <main className="text-white">
