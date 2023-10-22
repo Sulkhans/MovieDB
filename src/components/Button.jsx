5d611511cda9aa5c072d538af20c28e73b950563
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-10-22 17:25:49 +0400

    Responsive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import Arrow from "../assets/arrow.svg?react";
 
 export const Button = (props) => {
   return (
-    <button className="flex items-center text-lg px-5 py-2 border-2 rounded-full border-white border-opacity-30 fill-white hover:bg-white hover:text-black hover:fill-black transition-all">
+    <button className="flex items-center text-lg px-5 py-1 border-2 rounded-full border-white border-opacity-30 fill-white hover:bg-white hover:text-black hover:fill-black transition-all">
       <Link to={props.path}>See more</Link>
       <Arrow className="w-5 h-5 ml-1 -mr-1" />
     </button>
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -31,7 +31,7 @@ export const Info = () => {
         <></>
       ) : (
         <div
-          className="bg-center bg-no-repeat bg-cover pt-16 px-6 min-h-screen flex lg:px-32"
+          className="bg-center bg-no-repeat bg-cover pt-16 px-6 min-h-screen flex xl:pt-10"
           style={{
             backgroundImage: `linear-gradient(to bottom, rgb(10, 10, 10, 0.3), transparent ), linear-gradient(to top, rgb(10, 10, 10) 30%, transparent), url('https://image.tmdb.org/t/p/original/${item.backdrop_path}`,
           }}
@@ -39,24 +39,24 @@ export const Info = () => {
           <div className="flex flex-col mt-6 lg:flex-row lg:items-center xl:px-28 2xl:px-56">
             <img
               loading="lazy"
-              className="w-60 rounded-lg self-center lg:w-80 2xl:w-1/3"
+              className="w-60 rounded-lg self-center lg:w-80 lg:mx-12 2xl:w-1/3"
               src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
             />
-            <div className="flex flex-col items-center lg:items-start lg:pl-14">
+            <div className="flex flex-col items-center lg:items-start 2xl:pl-16">
               <h1 className="text-2xl py-6 text-center lg:text-start lg:text-5xl 2xl:text-8xl">
                 {item.title || item.name}
               </h1>
-              <div className="flex gap-4 justify-center">
+              <div className="flex gap-4 justify-center flex-wrap">
                 {item.genres.map((genre) => (
                   <h2
                     key={genre.id}
-                    className="px-4 py-2 border rounded-full self-center 2xl:text-3xl"
+                    className="px-4 py-2 border rounded-full self-center 2xl:text-2xl"
                   >
                     {genre.name}
                   </h2>
                 ))}
               </div>
-              <div className="w-80 grid grid-rows-2 grid-cols-2 justify-items-center gap-y-2 my-6 text-lg lg:justify-items-start 2xl:text-3xl 2xl:w-96">
+              <div className="w-80 grid grid-rows-2 grid-cols-2 justify-items-center gap-y-2 my-6 text-lg lg:justify-items-start 2xl:text-2xl 2xl:w-96 2xl:py-4">
                 <h2>Release date:</h2>
                 <h2>{item.release_date || item.first_air_date}</h2>
                 <h2>Runtime:</h2>
@@ -76,9 +76,9 @@ export const Info = () => {
                 <h2>Rating:</h2>
                 <h2>{item.vote_average.toFixed(1)}</h2>
               </div>
-              <h2 className="italic opacity-60 2xl:text-5xl">{item.tagline}</h2>
-              <div className="self-start py-6 sm:px-16 lg:px-0 2xl:text-3xl">
-                <h2 className="font-medium text-xl pb-2 2xl:text-4xl">
+              <h2 className="italic opacity-60 2xl:text-4xl">{item.tagline}</h2>
+              <div className="self-start py-6 sm:px-16 lg:px-0 2xl:text-2xl">
+                <h2 className="font-medium text-xl pb-2 2xl:text-4xl 2xl:py-4">
                   Overview
                 </h2>
                 <p>{item.overview}</p>
