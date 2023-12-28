5c43dc0ec4be322237ddccf55bc3ca198d3cc503
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-12-28 14:40:09 +0400

    Style fix

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -19,9 +19,9 @@ export const Slider = ({ latestMovies, index, setIndex }) => {
           {latestMovies[index].title}
         </h1>
         <div className="flex items-center">
-          <div className="flex items-center border-2 rounded-full bg-white text-black font-medium px-3 py-2 mr-3">
-            <Star className="w-4 h-4 mr-1 mb-[3px]" />
-            {latestMovies[index].vote_average}
+          <div className="flex items-center border-2 rounded-full border-white bg-white text-black font-medium px-3 py-2 mr-3">
+            <Star className="w-4 h-4 mr-1 mb-[2px]" />
+            {latestMovies[index].vote_average.toFixed(1)}
           </div>
           <Link to={`/MovieDB/movie/${latestMovies[index].id}}`}>
             <button className="px-6 py-2 border-2 rounded-full border-white border-opacity-30 hover:bg-white hover:text-black transition-all">
